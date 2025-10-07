60d539349f82427c8f143e8583e7d969cb6fc397
matteo@MBP14 <->
2025-10-07 17:53:21 +0200

    test color light/dark mode

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,12 @@ exclude_patterns = []
 html_theme = 'furo'
 
 html_theme_options = {
-    "css_variables": {
+    "light_css_variables": {
+        "color-brand-primary": "#BF69BB", #pink from yambo website
+        "color-brand-content": "#BF69BB",
+    },
+    #
+    "dark_css_variables": {
         "color-brand-primary": "#BF69BB", #pink from yambo website
         "color-brand-content": "#BF69BB",
     },
