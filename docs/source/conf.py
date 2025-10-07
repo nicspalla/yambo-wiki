6d2e02b23d296b170aa9afde972a8ab82d1137bb
matteo@MBP14 <->
2025-10-07 17:02:00 +0200

    test furo theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,30 +23,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_theme_options = {
-        'link'           : '#bf69bb',
-        'sidebar_link'   : '#bf69bb',
-        'sidebar_header' : '#471c46',
-        'pre_bg'         : '#eeeeee',
-        'warn_bg'        : '#ffffff',
-        'warn_border'    : '#cc3a35',
-        'note_bg'        : '#ffffff',
-        'note_border'    : '#0693e3',
-        'seealso_bg'     : '#ffffff',
-        'seealso_border' : '#00d084',
-        #
-        'narrow_sidebar_bg'   : '#471c46',
-        'narrow_sidebar_fg'   : '#ffffff',
-        'narrow_sidebar_link' : '#bf69bb',
-        #
-        'head_font_family'    : 'monospace',
-        'font_family'         : 'monospace',
-        'caption_font_family' : 'monospace',
-        'font_size'           : '15px',
-        'code_font_size'      : '15px',
-        #
-        'fixed_sidebar'   : True,
-        'show_powered_by' : False,
-}
+html_theme = 'furo'
+html_static_path = ['_static']
+html_css_files = ['custom.css']
 
